e0261e427423f17eaf87a110a68fc1133e31a127
Tracy Poff <tracy.poff@gmail.com>
2014-04-10 15:37:01 -0400

    Initial commit.
    Needs cleanup, but some support for Megapede, Kobo Deluxe, and Dangerous Dave.

---
+++ formats/Megapede.py
@@ -0,0 +1,120 @@
+"""Parses Megapede data."""
+import logging
+import struct
+import os
+from collections import namedtuple
+
+import yapsy
+from jinja2 import Environment, FileSystemLoader
+
+import utilities
+from errors import UnsupportedSoftwareException
+from utilities import File, FileReader
+
+
+env = Environment(loader=FileSystemLoader('./formats'))
+
+class Megapede(yapsy.IPlugin.IPlugin):
+    """Parses Megapede data."""
+
+    key = "cheesy_megapede_a"
+    title = "Megapede"
+    developer = "Cheesy Software"
+    description = "Megapede (Cheesy Software) v1.3c"
+
+    identifying_files = [
+        File("MEGAPEDE.EXE", 89844,  "60de7f0f904c0edd86c6ac4069e109f6"),
+        File("MEGACORE.EXE", 126448, "fce9aa8e9937211874efdbe8d88260be"),
+    ]
+    scorefile = "MEGAPEDE.SCO"
+
+    score_reader = FileReader([
+        ("name", "10s"),
+        ("padding", "x"),
+        ("score", "I"),
+        ("level", "B"),
+        ])
+
+    fmt = (
+        "<" +   # little-endian
+        "10s"   # name 1
+        "x"     # unknown
+        "I"     # score 1
+        "B"     # level 1
+        * 10    # scores 2-10
+    )
+
+    @staticmethod
+    def export(path, format="html"):
+        """Exports everything this class supports."""
+        if not Megapede.verify(path):
+            raise UnsupportedSoftwareException
+        global env
+        if format == "html":
+            template = env.get_template('Megapede.html')
+        text = template.render({
+            "key" : Megapede.key,
+            "title" : Megapede.title,
+            "developer" : Megapede.developer,
+            "description" : Megapede.description,
+            "high_score_table" : Megapede.read_scores(path),
+            "resources" : Megapede.extract_rd(path),
+            })
+        return text
+
+    @staticmethod
+    def verify(path):
+        """Verifies that the provided path is the supported game."""
+        return utilities.verify(Megapede.identifying_files, path)
+
+    @staticmethod
+    def read_scores(path):
+        """Reads score file foound in path."""
+        scores = []
+        with open(os.path.join(path, Megapede.scorefile), "rb") as scorefile:
+            for data in iter(lambda: scorefile.read(Megapede.score_reader.struct.size), b""):
+                scores.append(Megapede.score_reader.unpack(data))
+        return scores
+
+    @staticmethod
+    def maketable(scorefile):
+        """Create a high score table as a list of namedtuples.
+
+        The layout is [(name, score, level), ...]
+
+        """
+
+        MegapedeScore = namedtuple("MegapedeScore", ["name", "score", "level"])
+        COLS = 3
+        ROWS = 10
+        table = [
+                MegapedeScore(
+                    data[i*COLS].decode("utf_8").rstrip('\0'),
+                    data[i*COLS + 1],
+                    data[i*COLS + 2])
+                for i in range(ROWS)
+                ]
+        return table
+
+    @staticmethod
+    def extract_resources(path):
+        """Extract resources from MEGAPEDE.RES."""
+        resources = Megapede.extract_rd(path)
+        with open(path + "MEGAPEDE.RES", "rb") as infile:
+            for res in resources:
+                infile.seek(res.offset)
+                data = infile.read(res.size)
+                with open("extracted/" + res.name, "wb") as outfile:
+                    outfile.write(data)
+
+    @staticmethod
+    def extract_rd(path):
+        """"Get list of resources from MEGAPEDE.RD."""
+        MegapedeResource = namedtuple("MegapedeResource", ["size", "offset", "name"])
+        resources = []
+        with open(os.path.join(path, "MEGAPEDE.RD"), "rb") as rd:
+            for line in rd:
+                items = line.split()
+                res = MegapedeResource(int(items[0]), int(items[1]), items[2].decode())
+                resources.append(res)
+        return resources
