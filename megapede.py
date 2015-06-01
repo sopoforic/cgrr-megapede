a835d6c5754b6e641efe7baf917825a35a5c1be6
Tracy Poff <tracy.poff@gmail.com>
2015-06-01 06:56:42 -0400

    Remove unused import.

--- megapede.py
+++ megapede.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with CGRR.  If not, see <http://www.gnu.org/licenses/>.
 """Parses Megapede data."""
-import logging
 import os
 import struct
 from collections import namedtuple
